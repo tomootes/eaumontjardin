5a27d55ec9c4ad3b240e9cdb5c7d839148199088
Tom Ootes <tom@ootes.io>
2022-05-10 17:57:37 +0200

    ,

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// main.js
+// var contentful = require('contentful');
+
+// var client = contentful.createClient({
+//     space: 'lah0012agjlk',
+//     accessToken: 'EuC-ZVzBh3Hg7-4oCNoObcCErPf_shNs0N0Yxm-UcqA',
+// });
+
+// client.getEntries().then(function (entries) {
+//     console.log('got stuff');
+//   // log the title for all the entries that have it
+//   entries.items.forEach(function (entry) {
+//     if (entry.fields.productName) {
+//       console.log(entry.fields.productName);
+//     }
+//   });
+// });
