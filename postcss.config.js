f6b9970623707a6d5c91ccded19ae133c936a1b8
Tom Ootes <tom@ootes.io>
2024-02-27 16:12:50 +0100

    new setup

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+
+module.exports = {
+    parser: 'postcss-scss',
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]
+}
